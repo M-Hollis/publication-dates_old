3253240003a242d484a3f7d6b7f5e60a992650cd
Morgan Hollis <mhollis@ras.ac.uk>
2018-02-21 23:50:19 +0000

    Testing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, send_file
 
 from rq import Queue
 from worker import conn
@@ -33,15 +33,24 @@ def form_post():
 
 @app.route('/download')
 def download():
-    csv = """"REVIEW_DATE","AUTHOR","ISBN","DISCOUNTED_PRICE"
-"1985/01/21","Douglas Adams",0345391802,5.95
-"1990/01/12","Douglas Hofstadter",0465026567,9.95
-"1998/07/15","Timothy ""The Parser"" Campbell",0968411304,18.99
-"1999/12/03","Richard Friedman",0060630353,5.95
-"2004/10/04","Randel Helms",0879755725,4.50"""
-    response = make_response(csv)
-    response.headers["Content-Disposition"] = "attachment; filename=books.csv"
-    return response
+	try:
+		return send_file("/"+pars.filename, attachment_filename='dates.csv')
+	except Exception as e:
+		return str(e)
+
+
+# def download():
+#     csv = """"REVIEW_DATE","AUTHOR","ISBN","DISCOUNTED_PRICE"
+# "1985/01/21","Douglas Adams",0345391802,5.95
+# "1990/01/12","Douglas Hofstadter",0465026567,9.95
+# "1998/07/15","Timothy ""The Parser"" Campbell",0968411304,18.99
+# "1999/12/03","Richard Friedman",0060630353,5.95
+# "2004/10/04","Randel Helms",0879755725,4.50"""
+    
+# 	response = make_response(csv)
+#     response.headers["Content-Disposition"] = "attachment; filename=dates.csv"
+# 	response.headers["Content-type"] = "text/csv"
+#     return response
 
 
 if __name__ == "__main__":
